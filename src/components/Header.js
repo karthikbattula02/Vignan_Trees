685dee8a19afa4ed0a4e7a2b43ef45f1605079c8
Raja <118993556+Raja1704@users.noreply.github.com>
2023-03-22 16:12:44 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import {Link} from "react-router-dom"
-
+import "../css/header.css"
 const Header = ()=>{
     return(
         <>
             <Link to = "/">
-                <img src="http://vignaniit.edu.in/images/Logo.jpg"/>
+            <div className="header">
+                <table >
+                    <tr>
+                      <td>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Vignan_logo.png/1028px-Vignan_logo.png" ></img>
+                      </td>
+                      <td><h1><a href="#" >Vignan's Institute of Information Technology</a></h1></td>
+                       <td>
+                         <div className="header-right">
+                          <a className="active" href="#home">Home</a>
+                          <a hrefN="#contact">Contact</a>
+                          <a href="#about">About</a>
+                          
+                     </div>
+                     </td>
+                     </tr>
+                 </table>
+            </div>
             </Link>
         </>
     )
