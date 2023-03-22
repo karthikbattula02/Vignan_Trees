95d7f0490d7f651b3bc5af5669ab0431796ae446
Raja <118993556+Raja1704@users.noreply.github.com>
2023-03-22 21:31:22 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,10 @@ const Header = ()=>{
                       <td>
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Vignan_logo.png/1028px-Vignan_logo.png" ></img>
                       </td>
-                      <td><h1><a href="#" >Vignan's Institute of Information Technology</a></h1></td>
+                      <td><h1>Vignan's Institute of Information Technology</h1></td>
                        <td>
                          <div className="header-right">
                           <a className="active" href="#home">Home</a>
-                          <a hrefN="#contact">Contact</a>
-                          <a href="#about">About</a>
                           
                      </div>
                      </td>
